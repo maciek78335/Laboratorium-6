591d3c9551523d1291e71f883c7b62fed8170bec
maciek78335 <73504119+maciek78335@users.noreply.github.com>
2020-12-07 22:03:19 +0100

    Update Zad2.c

--- Zad2.c
+++ Zad2.c
@@ -1,39 +1,48 @@
 #include<stdio.h>
-#include<stdlib.h>
-#include<sys/types.h>
-#include<time.h>
 #include "mpi.h"
 
-float fx(float x){
+float f(float x)
+{
 return x*x;
 }
 
 int main(int argc, char **argv)
 {
-int rank, size;
-int a, b, n, i, j, p, r;
-MPI_Comm_size(MPI_COMM_WORLD, &size);
-MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-srand(time(NULL));
-printf("Podaj p: ");
-scanf("%d", &p);
-
-for(i=0; i<=p; i++)
+int r, s;
+MPI_Init(&argc, &argv);
+MPI_Comm_size(MPI_COMM_WORLD, &s);
+MPI_Comm_rank(MPI_COMM_WORLD, &r);
+MPI_Status st;
+  
+float res = 0;
+float b = 10;
+float a = 0;
+float dx = (b-a)/s;
+if(r != s-1)
 {
-n = rand()%10;
-a = rand()%10;
-b = rand()%10+10;
-float c = 0;
-float h = (b-a)/n;
-for(int j=1; j<= n-1; j++)
+  MPI_Recv(&res, 1, MPI_FLOAT, r+1, 25, MPI_COMM_WORLD, &st);
+}
+if(r+1 == s)
 {
-c += fx(a+1*h);
+  res = res + f(b)/2;
 }
-c += (fx(a) + fx(b))/2;
-c *= h;
-printf("%f", c);
+else if(r == 0)
+{
+  res = res + f(a)/2;
 }
-printf("process %d of %d", rank, size);
+else
+{
+  res = res + f(a+r*dx);
+}
+  
+if(r!=0)
+{
+  MPI_Send(&res, 1, MPI_FLOAT, r-1, 25, MPI_COMM_WORLD);
+}
+else
+{
+  printf("%f\n", res*dx);
+}
 MPI_Finalize();
 return 0;
 }
